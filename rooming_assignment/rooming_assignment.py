d640202381dc61eb90db4eaf5d27896b9397809b
Will Oursler <woursler@mit.edu>
2014-01-29 12:15:57 -0500

    MVP rooming_assignment, without templates (unicode string representations may cause problems.)

---
+++ rooming_assignment/rooming_assignment.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python
+
+# Add the Simmons DB utils to the PYTHONPATH (temporary).
+import sys, os
+sys.path.append( os.path.abspath( os.path.join(sys.path[0], '../utils') ) )
+
+from sdb import Resident, sdb_session
+
+# Setup flask basics.
+from flask import Flask, render_template, make_response
+app = Flask(__name__)
+
+# BEGIN HELPERS ###########################################
+
+# TODO: fix DoS vulnerabilties.
+# TODO: Used in people.py as well, perhaps abstract to sdb?
+
+def get_people():
+	session = sdb_session()
+
+	# Make a full list of people of interest...
+	people = []
+	for person in session.query(Resident):
+		if person.private:
+			continue
+		people.append( {
+			'kerberos'	:person.username,
+			'firstname'	:person.firstname,
+			'lastname'	:person.lastname,
+			'room'		:person.room,
+			'year'		:person.year,
+			'title'		:person.title,
+			'email'		:person.email,
+			} )
+
+		
+	session.close()
+
+	return people
+
+def get_person_room( username ):
+	people = get_people()
+	for person in people:
+		if person['kerberos'] == username:
+			return person['room']
+
+def get_room_people( room ):
+	result = []
+	for person in get_people():
+		if person['room'] == room:
+			result.append( person['kerberos'] )
+	return result
+
+# END HELPERS #############################################
+
+# BEGIN API ###############################################
+
+@app.route('/person/<username>/')
+def serve_person_room( username ):
+	return str( get_person_room( username ) ) # TODO: Template
+
+@app.route('/room/<roomnum>/')
+def serve_room_people( roomnum ):
+	return str( get_room_people( roomnum ) ) # TODO: Template
+
+# END API #################################################
+
+# RUN FLASK APP ###########################################
+
+if __name__ == "__main__":
+	app.debug = True # TODO: Remove in production.
+    	app.run()
+
+# END FLASK APP ###########################################
