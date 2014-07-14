ad4dee9d238426167838905d6e530d9527be2ee7
Amol Bhave <ammubhave@gmail.com>
2014-07-13 19:52:32 -0700

    Nuke Cosmos

--- src/rooms/rooms.py
+++ src/rooms/rooms.py
@@ -1,25 +1,33 @@
 #!/usr/bin/python
 
 # Setup flask basics.
+from ..utils import db
 from flask import Flask, jsonify
 app = Flask(__name__)
 
-import cosmos_db
 
 @app.route('/')
 def serve_rooms():
-	roomnums = [ room.num for room in cosmos_db.allRooms ]
+	room_db = db.init('room')
+	rooms = room_db.query(db.Room).all()
+	roomnums = [ room.num for room in rooms ]
 	return jsonify( rooms = roomnums )
 
 @app.route('/<roomnum>/')
 def serve_room( roomnum ):
-	room = cosmos_db.find( roomnum )
+	room_db = db.init('room')
+	room = room_db.query(db.Room).get(roomnum)
+
 	return jsonify(
 		roomnum = room.num,
-		grt = room.grt,
-		capacity = room.capacity,
-		sq_ft = room.size,
-		side = room.view )
+		type = room.type,
+		size = room.size,
+		view = room.view,
+		X = room.X,
+		Y = room.Y,
+		hasCurvyWall = room.hasCurvyWall,
+		bathroom = room.bathroom
+	)
 
 if __name__ == "__main__":
 	app.debug = True # TODO: Remove in production.
