1cb0edcd4c2fce83a2e5ad670debaaab383da2aa
Will Oursler <woursler@mit.edu>
2014-08-06 09:22:46 -0700

    Move all testing related things into test/, begin to move them to unittest

---
+++ test/roomDB.py
@@ -0,0 +1,11 @@
+import unittest
+
+class TestRoomDBIntegrity(unittest.TestCase):
+	def setUp(self):
+		# TODO: Download all rooms!
+
+	def test_view(self):
+		self.assertTrue(false)
+
+if __name__ == '__main__':
+	unittest.main()
