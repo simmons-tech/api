8fd140518ce42c3224530938e1503e2fad66583b
Amol Bhave <ammubhave@gmail.com>
2015-02-15 22:02:41 -0500

    make officers work

--- src/officers/officers.py
+++ src/officers/officers.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python
 
-from ..utils.sdb import Officers, sdb_session
+from ..utils.sdb import Officers, sdb_session, Resident
 
 # Setup flask basics.
 from functools import wraps
-from flask import Flask, request, jsonify, redirect, current_app
+from flask import Flask, request, jsonify, redirect, current_app, json
 app = Flask(__name__)
 
 # TODO: fix DoS vulnerabilties.
@@ -19,17 +19,37 @@ def get_officers():
 	session = sdb_session()
 	# Make a full list of people of interest...
 	officers = []
-	for officer in session.query(Officers, removed=None):
-		officers.append( get_person(officer['username']) )
+	#print 'querying...'
+	#print session.query(Officers)
+	for officer in session.query(Officers).filter(Officers.removed == None):
+		#print officer
+		#print officer.username
+		#try:
+		for person in session.query(Resident).filter(Resident.username == officer.username):
+			officers.append( {
+				'kerberos'	:person.username,
+				'firstname'	:person.firstname,
+				'lastname'	:person.lastname,
+				'year'		:person.year,
+				#'title'		:person.title,
+				'email'		:person.email,
+				'position': officer.position
+				} )
+		#except Exception, ex:
+		#	print ex
 		
 	session.close()
+	#print officers, ' added'
 
 	return officers
 
 @app.route('/')
 def serve_officers():
+	#try:
 	officers = get_officers()
-	return jsonify( officers )
+	return json.dumps(get_officers())
+	#except Exception, ex:
+		#print ex
 
 if __name__ == "__main__":
 	app.debug = True # TODO: Remove in production.
